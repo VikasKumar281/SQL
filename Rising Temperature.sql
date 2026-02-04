0aa6752ef4a666ec15798423e1022945a4ca3375
Vikas Kumar <vikaskumar280204@gmail.com>
2026-02-04 16:40:14 +0530

    Update query to use JOIN instead of self join
    Refactor self join query to use JOIN syntax.

--- Rising Temperature.sql
+++ Rising Temperature.sql
@@ -1,3 +1,11 @@
+-- // Self Join ---------------------------------------->
+select w1.id
+from weather w1 , weather w2
+where datediff(w1.recordDate, w2.recordDate) = 1
+and w1.temperature > w2.temperature;
+
+
+
 SELECT w1.id
 FROM Weather w1
 JOIN Weather w2
