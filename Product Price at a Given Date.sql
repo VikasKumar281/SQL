84313ff293108f9ffde3715eb55586122ac6ab38
Vikas Kumar <vikaskumar280204@gmail.com>
2026-04-06 18:14:54 +0530

    Create Product Price at a Given Date.sql

---
+++ Product Price at a Given Date.sql
@@ -0,0 +1,14 @@
+SELECT p1.product_id,
+       IFNULL(
+           (
+               SELECT p2.new_price
+               FROM Products p2
+               WHERE p2.product_id = p1.product_id
+                 AND p2.change_date <= '2019-08-16'
+               ORDER BY p2.change_date DESC
+               LIMIT 1
+           ),
+           10
+       ) AS price
+FROM Products p1
+GROUP BY p1.product_id;
